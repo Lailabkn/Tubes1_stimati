e81ae7798f02653a6340fc79327871676dcaab49
syauqijan <13521014@std.stei.itb.ac.id>
2023-02-17 23:36:31 +0700

    last update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,7 +58,7 @@ public class Main {
 
         Thread.sleep(1000);
         System.out.println("Registering with the runner...");
-        hubConnection.send("Register", token, "Stimati aksa");
+        hubConnection.send("Register", token, "Stimati");
 
         //This is a blocking call
         hubConnection.start().subscribe(() -> {
